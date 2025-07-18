e145a169bbfafe9f34ac0312758b257eaa4a5022
ski232323 <ulysse.renzetti@icloud.com>
2025-07-18 09:22:24 +0200

    cool

--- token.sol
+++ token.sol
@@ -2,31 +2,32 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.27;
 
-import {ERC1363} from "@openzeppelin/contracts/token/ERC20/extensions/ERC1363.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Bridgeable} from "@openzeppelin/community-contracts/token/ERC20/extensions/ERC20Bridgeable.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import {ERC20FlashMint} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
-import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Testcoin is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC1363, ERC20Permit, ERC20Votes, ERC20FlashMint {
-    constructor(address recipient, address initialOwner)
-        ERC20("Testcoin", "TST")
+contract TESTCOIN is ERC20, ERC20Bridgeable, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
+    address public immutable TOKEN_BRIDGE;
+    error Unauthorized();
+
+    constructor(address tokenBridge, address recipient, address initialOwner)
+        ERC20("TESTCOIN", "TST")
         Ownable(initialOwner)
-        ERC20Permit("Testcoin")
+        ERC20Permit("TESTCOIN")
     {
-        _mint(recipient, 1000 * 10 ** decimals());
+        require(tokenBridge != address(0), "Invalid TOKEN_BRIDGE address");
+        TOKEN_BRIDGE = tokenBridge;
+        if (block.chainid == 11155111) {
+            _mint(recipient, 10 * 10 ** decimals());
+        }
     }
 
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
+    function _checkTokenBridge(address caller) internal view override {
+        if (caller != TOKEN_BRIDGE) revert Unauthorized();
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
@@ -37,7 +38,7 @@ contract Testcoin is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC1363, ERC2
 
     function _update(address from, address to, uint256 value)
         internal
-        override(ERC20, ERC20Pausable, ERC20Votes)
+        override(ERC20, ERC20Votes)
     {
         super._update(from, to, value);
     }
